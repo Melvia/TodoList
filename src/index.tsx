c285300c6cb80d2e00a665ec444a13ac683a176c
Nata <melvia.nr@gmail.com>
2022-02-04 20:00:33 +0500

    commit befote pull

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import store from "./redux/store.ts";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./components/App/App.tsx";
-import About from "./pages/about";
-import Error from "./pages/error";
+import About from "./pages/about.tsx";
+import Error from "./pages/error.tsx";
 import SiteRoutes from "./constants/url.ts";
 
 const { ABOUT, ERROR, APP } = SiteRoutes;
